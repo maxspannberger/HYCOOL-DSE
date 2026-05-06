2fdd8a534ed84fdf3edbd9d0139aabe82b031dc7
tababei <tababeipetrea@tudelft.nl>
2026-05-06 11:27:17 +0200

    Matching diagram class started

---
+++ WeightEstimations/objects.py
@@ -0,0 +1,8 @@
+import numpy as np
+import scipy as sp
+
+# this file will contain all the classes that are used in this Class I estimation
+
+class MatchingDiagram:
+    def __init__(self, TO_field_length):
+        self.TO_field_length = TO_field_length
