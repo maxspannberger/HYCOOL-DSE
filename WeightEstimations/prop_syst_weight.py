a507d1732014e2462f5267f36da3324021bd372c
maxspannberger <m.spannberger@student.tudelft.nl>
2026-05-11 17:20:17 +0200

    added power from class 2 estimation

--- WeightEstimations/prop_syst_weight.py
+++ WeightEstimations/prop_syst_weight.py
@@ -1,18 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 from pathlib import Path
-root = Path(_file_).resolve().parent.parent
+root = Path(__file__).resolve().parent.parent
 sys.path.append(str(root))
 from General.component_parameters import component_params as c
+from mainClassII import default_q400_hycool, run_class_ii
 
+# Run Class II once and reuse the config/result
+cfg = default_q400_hycool()
+class_ii_result = run_class_ii(cfg, verbose=False)
 
 #--------------------- INPUTS ------------------------- (these will be changed to take proper variables from other files)
-P_cruise    =   3.8  #MW 
-P_climb     =   5.1  #MW
-P_reserve   =   1.3  #MW
-P_TO_OEI    =   3.09 #MW
 
-N_propulsors = 2
+P_cruise    =   class_ii_result.mission.P_cruise_shaft/ 1e6  #MW 
+P_climb     =   class_ii_result.mission.P_climb_shaft/ 1e6  #MW
+P_reserve   =   class_ii_result.mission.P_reserve_shaft/ 1e6  #MW
+
+# Use per-engine takeoff power (watts) and compute OEI power in MW
+P_TO_OEI =  class_ii_result.power.P_TO_per_engine/ 1e6  # MW
+print(f"Per-engine takeoff power: {P_TO_OEI:.2f} MW")
+print(f"Climb power: {P_climb:.2f} MW")
+print(f"Cruise power: {P_cruise:.2f} MW")
+print(f"Reserve power: {P_reserve:.2f} MW")
 
 #--------------------- Power/energy densities -------------------------
 PD_GT       =   c["gt"].power_density  #kW/kg Power density of gas turbine
