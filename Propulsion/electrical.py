c6b3646187d13673c5fea9502d52601466fcec52
AlexCostache267 <alex.costache267@gmail.com>
2026-06-04 14:35:15 +0200

    EPS architecture update

--- Propulsion/electrical.py
+++ Propulsion/electrical.py
@@ -18,177 +18,40 @@ R_th_26 = 14.3  # K/kW
 R_th = R_th_26/26
 
 cable_loss_per_m = 0.0 # kW/m
-P_AC_systems = 0.0  # kW
+P_AC_systems = 310.0  # kW
 
 
-def get_cable_region_powers(P_out_branch, positions, condition):
-    """
-    Calculate power carried by every cable region in the aircraft.
+def get_cable_region_powers(N_motors, N_turbines, component, positions, previous):
+    comp_powers = {}
+    maximum = {}
+    if "in" in component:
+        for pos in positions["gt"]:
+            comp_powers[pos] = {}
+            comp_powers[pos]["length"] = abs(pos - positions["bus"])
+            for condition in ["max", "cruise", "OEI_mot", "OEI_gt"]:
+                comp_powers[pos][condition] = previous[condition] * (1.0 - cable_loss_per_m * comp_powers[pos]["length"])
+                if condition not in maximum:
+                    maximum[condition] = 0.0
+                if comp_powers[pos][condition] > maximum[condition]:
+                    maximum[condition] = comp_powers[pos][condition]
 
-    Parameters
-    ----------
-    P_total : float
-        Total propulsion power required [kW]
-
-    positions : dict
-        Example:
-        {
-            "gt": [5],
-            "mot": [10, 15]
-        }
-
-        Values are half-wing positions [m].
-
-    condition : str
-        "max"
-        "cruise"
-        "OEI_mot"
-        "OEI_gt"
-
-    Returns
-    -------
-    regions : list(dict)
-        Example:
-        [
-            {
-                "span": (-15,-10),
-                "length": 5,
-                "power": 1000,
-                "loss": 5
-            },
-            ...
-        ]
-
-    total_loss : float
-        Sum of all cable losses [kW]
-    """
-
-    # --------------------------------------------------
-    # Build full-aircraft component locations
-    # --------------------------------------------------
-    full_gt_positions = (
-        [-x for x in reversed(positions["gt"])]
-        + positions["gt"]
-    )
-    full_mot_positions = (
-        [-x for x in reversed(positions["mot"])]
-        + positions["mot"]
-    )
-
-    N_gt = len(full_gt_positions)
-    N_mot = len(full_mot_positions)
-
-    if condition == "OEI_mot":
-        active_gt = N_gt
-        active_mot = N_mot - 1
-
-    elif condition == "OEI_gt":
-        active_gt = N_gt - 1
-        active_mot = N_mot
+    elif "out" in component:
+        for pos in positions["mot"]:
+            comp_powers[pos] = {}
+            comp_powers[pos]["length"] = abs(pos - positions["bus"])
+            for condition in ["max", "cruise", "OEI_mot", "OEI_gt"]:
+                comp_powers[pos][condition] = previous[condition] * (1.0 - cable_loss_per_m * comp_powers[pos]["length"])
+                if condition == "OEI_mot":
+                    maximum[condition] = previous[condition] * (N_motors - 1) / 2
+                elif condition == "OEI_gt":
+                    maximum[condition] = previous[condition] * N_motors
+                else:
+                    maximum[condition] = previous[condition] * N_motors / 2
 
     else:
-        active_gt = N_gt
-        active_mot = N_mot
+        raise ValueError("There are only 2 conditions for cabling: before bus (1) and after bus (2).")
 
-    nodes = full_mot_positions + [0.0]
-    node_losses = {x: 0.0 for x in nodes}
-    old_loss = np.inf
-    total_loss = 0.0
-    i = 0
-
-    while np.abs(total_loss - old_loss) > 1e-6 and i < 100:
-        old_loss = total_loss
-        i += 1
-
-        P_in_branch = (P_out_branch * active_mot + P_AC_systems + total_loss) / active_gt 
-
-        # --------------------------------------------------
-        # Build injection list
-        # + generation
-        # - load
-        # --------------------------------------------------
-        injections = []
-
-        # --------------------------
-        # Turbines
-        # --------------------------
-        gt_locations = full_gt_positions.copy()
-        if condition == "OEI_gt":
-            # remove first turbine
-            # (later you can replace with worst-case selection)
-            gt_locations.pop(0)
-        for x in gt_locations:
-            injections.append((x, P_in_branch))
-
-        # --------------------------
-        # Motors
-        # --------------------------
-        mot_locations = full_mot_positions.copy()
-
-        if condition == "OEI_mot":
-            # remove first motor
-            # (later replace by worst-case motor)
-            mot_locations.pop(0)
-
-        for x in mot_locations:
-            P_load = P_out_branch + node_losses.get(x, 0.0)
-            injections.append((x, -P_load))
-
-        # --------------------------
-        # AC systems at fuselage
-        # --------------------------
-        P_out_system = P_AC_systems + node_losses.get(0.0, 0.0)
-        injections.append((0.0, -P_out_system))
-
-        # --------------------------------------------------
-        # Sort by spanwise position
-        # --------------------------------------------------
-        injections.sort(key=lambda item: item[0])
-
-        # --------------------------------------------------
-        # Power flow through each region
-        # Flow in region i:
-        # absolute cumulative injection
-        # to the left of the region.
-        # --------------------------------------------------
-
-        power_flow = 0.0
-        regions = []
-        total_loss = 0.0
-        node_losses = {x: 0.0 for x in node_losses}
-
-        for i in range(len(injections) - 1):
-            x_left = injections[i][0]
-            x_right = injections[i + 1][0]
-            power_flow += injections[i][1]
-
-            length = abs(x_right - x_left)
-            loss = (
-                abs(power_flow)
-                * cable_loss_per_m
-                * length
-            )
-            total_loss += loss
-
-            regions.append(
-                {
-                    "span": (x_left, x_right),
-                    "length": length,
-                    "power": power_flow,
-                    "loss": loss,
-                }
-            )
-
-            if power_flow > 1e-6:
-                node_distances = [node - x_right for node in nodes]
-                x_out = x_right + min([node for node in node_distances if node >= -1e-6])
-                node_losses[x_out] += loss
-            elif power_flow < -1e-6:
-                node_distances = [node - x_left for node in nodes]
-                x_out = x_left + max([node for node in node_distances if node <= 1e-6])
-                node_losses[x_out] += loss
-
-    return regions, total_loss
+    return comp_powers, maximum
 
 
 def get_powers_per_component(P_max, P_cruise, P_OEI, positions, component_order, comp):
@@ -199,13 +62,6 @@ def get_powers_per_component(P_max, P_cruise, P_OEI, positions, component_order,
 
     N_motors = len(positions["mot"]) * 2
     N_turbines = len(positions["gt"]) * 2
-
-    operating_conditions = {
-        "max": P_max,
-        "cruise": P_cruise,
-        "OEI_mot": P_OEI,
-        "OEI_gt": P_OEI,
-    }
 
     # -------------------------------------------------------------
     # Initial power demand at motor outputs
@@ -224,54 +80,15 @@ def get_powers_per_component(P_max, P_cruise, P_OEI, positions, component_order,
     # ------------------------------------------------------------
 
     for component in reversed(component_order):
+        comp_powers[component] = {}
 
-        # ---------------------------------------------------------
-        # Cable section
-        # ---------------------------------------------------------
-        if component == "cable":
-            comp_powers["cable"] = {}
-            cable_losses = {}
+        if "cable" in component:
+            comp_powers_cable, previous = get_cable_region_powers(N_motors, N_turbines, component, positions, previous)
+            comp_powers[component] = comp_powers_cable
 
-            for condition, power_required in previous.items():
-                regions, total_loss = get_cable_region_powers(
-                    P_out_branch=power_required,
-                    positions=positions,
-                    condition=condition
-                )
-                comp_powers["cable"][condition] = {
-                    "regions": regions,
-                    "total_loss": total_loss,
-                }
-                cable_losses[condition] = total_loss
-
-            # Add cable losses to required upstream power
-            previous = {
-                cond: previous[cond] + cable_losses[cond]
-                for cond in previous
-            }
-
-            # Adjust for turbine/motor count change
-            # across electrical distribution system
-
-            previous["max"] *= N_motors / N_turbines
-            previous["cruise"] *= N_motors / N_turbines
-            previous["OEI_mot"] *= (
-                (N_motors - 1) / N_turbines
-            )
-            previous["OEI_gt"] *= (
-                N_motors / (N_turbines - 1)
-            )
-
-        # ---------------------------------------------------------
-        # Efficiency component
-        # ---------------------------------------------------------
         else:
-            comp_powers[component] = {}
             for condition in previous:
-                comp_powers[component][condition] = (
-                    previous[condition]
-                    / comp[component].efficiency
-                )
+                comp_powers[component][condition] = previous[condition] / comp[component].efficiency
             previous = comp_powers[component].copy()
 
     return comp_powers
@@ -303,16 +120,19 @@ def size_converter(comp, powers):
     T_J_OEI = T_H2 + get_deltaT(P_OEI, eff, N)
     P_heat_idle = get_P_idle(T_J_min, N)
 
-    print(N, T_J_cruise, T_J_OEI, P_heat_idle)
+    P_max = max(P_OEI, powers["max"])
+    m = P_max / comp.power_density
 
-    return N, T_J_cruise, T_J_OEI, P_heat_idle
+    print(N, T_J_cruise, T_J_OEI, P_heat_idle, m)
+
+    return N, T_J_cruise, T_J_OEI, P_heat_idle, m
 
 
 
 if __name__ == "__main__":
     # define electrical system architecture
-    component_order = ["gt_hex", "hts_gen", "ac_dc", "cable", "dc_ac", "hts_pow"]
-    positions = {"gt": [5], "mot": [10, 15]}
+    component_order = ["gt_hex", "hts_gen", "ac_dc", "cable_in", "bus", "cable_out", "dc_ac", "hts_pow"]
+    positions = {"gt": [5], "mot": [10, 15], "bus": 3}
 
     # get class II power results
     print("Performing Class II estimations...")
@@ -330,8 +150,10 @@ if __name__ == "__main__":
         json.dump(powers, f, indent=4)
     print("Electrical system power sizing complete.")
 
-    P_inverter = size_converter(comp_params["dc_ac"], powers["dc_ac"])[3]
-    P_rectifier = size_converter(comp_params["ac_dc"], powers["ac_dc"])[3]
+    _, _, _, P_inverter, m_inverter = size_converter(comp_params["dc_ac"], powers["dc_ac"])
+    _, _, _, P_rectifier, m_rectifier = size_converter(comp_params["ac_dc"], powers["ac_dc"])
     P_total_idle = 2*len(positions["mot"]) * P_inverter + 2*len(positions["gt"]) * P_rectifier
+    m_total = 2*len(positions["mot"]) * m_inverter + 2*len(positions["gt"]) * m_rectifier
     print(f"Total heating power required for idle: {P_total_idle}")
+    print(f"Total mass of converters: {m_total}")
     print("Electrical components sizing complete.")
