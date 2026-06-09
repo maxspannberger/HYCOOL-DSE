7f4a5de3d102093fe6d9177d98282a5e873361cd
Patrick <pblok@tudelft.nl>
2026-06-09 09:45:57 +0200

    config value changes

--- WeightEstimations/Aircraft_Config.py
+++ WeightEstimations/Aircraft_Config.py
@@ -219,7 +219,7 @@ def default_q400_hycool() -> AircraftConfig:
         tc_h             = 0.12,                # Referenced
         sweep_h_half     = np.deg2rad(22.0),    # Referenced
         sweep_h_tc       = np.deg2rad(20.0),    # Referenced
-        l_h              = 20.4,                # Referenced
+        l_h              = 19.8,                # Referenced
 
         # Vertical tail
         MAC_v            = MAC_v,               
@@ -319,11 +319,11 @@ def default_q400_hycool() -> AircraftConfig:
         FirstWindow         = 6.74,                 # Distance nose tip to first window [m], placeholder for now
         LastWindow          = 27.08,                # Distance nose tip to last window [m], placeholder for now
 
-        LEMAC               = 15.5,                 # Distance nose tip to LEMAC [m], placeholder for now
+        LEMAC               = 16.1,                 # Distance nose tip to LEMAC [m], placeholder for now
         lfn                 = 14,                 # Distance nose tip to LE wing root
         hh                  = 4,                    # Normal distance from wing plane to tail plane, placeholder for now
 
-        OEW_cg              = 16.46,                # Distance nose tip to OEW CG [m], placeholder for now
+        OEW_cg              = 16.74,                # Distance nose tip to OEW CG [m], placeholder for now
         FUEL_cg             = 30,                   # Distance nose tip to Fuel CG [m], placeholder for now
         AftCargo_cg         = 25,                   # Distance nose tip to Aft Cargo CG [m], placeholder for now
         FwdCargo_cg         = 10,                   # Distance nose tip to Fwd Cargo CG [m], placeholder for now
@@ -344,6 +344,6 @@ def default_q400_hycool() -> AircraftConfig:
         cg_surf_control     = 1,                    # 100% of MAC from LEMAC
 
         cg_location_engines = 0.5,                  # [m] from LEMAC to cg of the power units on the wing
-        OEW_target_rel      = 0.3,                 # % of MAC, from LEMAC. Value for config 3 (wing mtd engines) from Torenbeek p.300 (range is 0.2-0.25)
+        OEW_target_rel      = 0.2,                 # % of MAC, from LEMAC. Value for config 3 (wing mtd engines) from Torenbeek p.300 (range is 0.2-0.25)
     
     )
