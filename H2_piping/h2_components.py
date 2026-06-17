Fixed area calculation code, temp calc still TODO

H2_piping/h2_components.py
@@ -666,31 +666,33 @@ class COOL:
                     deltaT = self.Q_dot / (U * self.area * HEX_effectiveness)
                     self.T = deltaT + 0.5 * (T1 + T2)
             '''
-            w_c = self.width * self.length / self.L # + t/2 # the t/2 is added only if the wing tip contributes
-            t_f = self.d + 2 * config.HEX_extra_thickness
-            R_f = w_c / (6 * config.k_Al * t_f * self.length)
-            # TODO: check this with a source or derivation
-            # print(R_f)
+
             Tb = 0.5 * (T1 + T2)
 
+            t_f = self.d + 2 * config.HEX_extra_thickness
+            R_f = self.width / (6 * config.k_Al * t_f * self.L)
+            # print(R_f)
+
+            R_tot = (self.T - Tb) / self.Q_dot * 2*self.L/self.length
+            # print(R_tot)
+
+            if "hts" not in self.name:
+                k_TMI = config.k_TMI_4 + (config.k_TMI_293 - config.k_TMI_4)/289 * (Tb - 4)
+                R_TMI = 2 * config.t_TMI * self.L / (k_TMI * self.length**2 * self.width)
+                # print(R_TMI)
+
             # TODO: double check the following code
-            if self.area_calc_mode:
-                R_tot = (self.T - Tb) / self.Q_dot
-                print(R_tot)
-                if "hts" not in self.name:
-                    k_TMI = config.k_TMI_4 + (config.k_TMI_293 - config.k_TMI_4)/289 * (Tb - 4)
-                    R_TMI = 2 * config.t_TMI / (k_TMI * self.length * self.width)
-                    print(R_TMI)
-                    R_tot -= R_TMI
-                
+            if self.area_calc_mode:                
                 Tw = max(self.T - self.Q_dot * R_f, Tb)
                 h_H2 = heat_transfer_coefficient(T1, T2, Tw, p1, p2, m_dot, self.d, self.fluid)
-                # if R_tot > R_f:
-                #     self.area = 1 / (h_H2 * (R_tot - R_f))
-                #     self.L = self.area / (self.N_channels * np.pi * self.d)
-                # else:
-                #     self.L = 1 / R_tot * (R_f * self.L + 1 / (np.pi * self.d * h_H2))
-                self.L = (1 / (6 * config.k_Al * t_f) + 2 / (np.pi * self.d * h_H2)) / R_tot
+
+                a = 2/self.length * (self.T - Tb) / self.Q_dot
+                b = -2 / (np.pi * self.d * self.length * h_H2)
+                c = -self.width / (6 * config.k_Al * t_f * self.length)
+                if "hts" not in self.name:
+                    a -= 2 * config.t_TMI / (k_TMI * self.width * self.length**2)
+
+                self.L = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
                 self.L = config.FPI_relaxation * self.L + (1.0 - config.FPI_relaxation) * L_old
                 self.area = np.pi * self.d * self.L
 
@@ -710,8 +712,8 @@ class COOL:
                     T_old = self.T
                     Tw = self.T - self.Q_dot * R_f
                     h_H2 = heat_transfer_coefficient(T1, T2, Tw, p1, p2, m_dot, self.d, self.fluid)
-                    R_tot = R_f + 1 / (self.area * h_H2)
-                    self.T = self.Q_dot * R_tot + Tb
+                    R_tot = R_f + 2 / (np.pi * self.d * self.length * h_H2)
+                    self.T = self.Q_dot * self.length/(2 * self.L) * R_tot + Tb
 
         if show:
             if self.area_calc_mode: